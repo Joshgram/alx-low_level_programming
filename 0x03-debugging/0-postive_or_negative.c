adceec561e58cc78e942e204722a08e517bb86f8
Joshgram <joshuadaramola28@gmail.com>
2022-07-07 03:27:01 -0700

    task 0

---
+++ 0x03-debugging/0-postive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	strand(time{0});
+	n = rand() - RAND MAX / 2;
+	if (n > 0)
+		printf("%d is postive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n > 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
